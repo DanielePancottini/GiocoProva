77b9c29bbc8856a0c367b35d23033917deb098e9
TaThan01 <45490869+TaThan01@users.noreply.github.com>
2018-12-04 17:27:09 +0100

    - aggiunta classe palla con attributi le coordinate della palla

---
+++ app/src/main/java/giocoprova/programmatorifalliti/it/giocoprova/Palla.java
@@ -0,0 +1,9 @@
+package giocoprova.programmatorifalliti.it.giocoprova;
+
+import android.graphics.Paint;
+
+public class Palla {
+    public Integer ballX;
+    public Integer ballY;
+    public Paint ballPaint = new Paint();
+}
