ae13f2167b3337205002967d8483fac475f3947b
DanielePancottini <danielepancottini@gmail.com>
2018-12-04 17:11:30 +0100

    - Aggiunta timer di 60s Play Activity

--- app/src/main/java/giocoprova/programmatorifalliti/it/giocoprova/PlayActivity.java
+++ app/src/main/java/giocoprova/programmatorifalliti/it/giocoprova/PlayActivity.java
@@ -1,13 +1,37 @@
 package giocoprova.programmatorifalliti.it.giocoprova;
 
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class PlayActivity extends AppCompatActivity {
+
+    public TextView counterText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play);
+
+        startCounter();
+
     }
+
+    public void startCounter(){
+        counterText = (TextView) findViewById(R.id.timerPlay);
+
+        CountDownTimer countDownTimer = new CountDownTimer(60*1000, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                counterText.setText("" + millisUntilFinished/1000);
+            }
+
+            @Override
+            public void onFinish() {
+                counterText.setText("MARCO GAY");
+            }
+        }.start();
+    }
+
 }
